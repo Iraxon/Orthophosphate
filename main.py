107fd6e8997786239be96447fb32f8f409540e00
Iraxon <156947097+Iraxon@users.noreply.github.com>
2024-12-19 17:29:12 -0500

    Began work on file selection in main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Eventual entry point of the program here
 
 import user_interface.file_utils
+from tkinter import Tk, filedialog
 
 if __name__ == "__main__":
-    pass
+
+    root = Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(filetypes=[("<ProjectName> Files", "*.txt")])
