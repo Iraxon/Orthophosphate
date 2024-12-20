e054f505cb67c57e6c9ef77414cd3c11f3cceea3
Iraxon <156947097+Iraxon@users.noreply.github.com>
2024-12-20 10:24:36 -0500

    More work on main.
    Tkinter is driving me insane.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tkinter import ttk
 from tkinter import filedialog
 
 # VOID, changes file_path Tkinter variable
-def browse_for_file():
+def browse_for_file(*args):
     """
     Opens system file dialog
     and assigns the user-selected
@@ -15,36 +15,47 @@ def browse_for_file():
     (which puts it in the entry box
     as well)
     """
-    global file_path
 
-    file_path_str = filedialog.askopenfilename(filetypes=[("<ProjectName> Files", "*.txt")])
+    file_path_str = filedialog.askopenfilename(filetypes=[("Orthophosphate Files", "*.txt *.orph")])
     
     if file_path_str:
         pass
     else:
         file_path_str = "No file selected"
-    file_path.set(file_path_str)
+    file_path_in.set(file_path_str)
 
 if __name__ == "__main__":
 
     root = Tk()
-    root.title("<ProjectName>")
+    root.title("Orthophosphate")
 
-    (mainframe := ttk.Frame(root, padding="3 3 12 12")).grid(column=0, row=0, sticky=NSEW) # Main grid;
+    (mainframe := ttk.Frame(root)).grid(column=0, row=0, sticky=NSEW) # Main grid;
     # Walrus operator := is just assignment. You use it instead of =
     # inside expressions. Python has this to prevent errors
     # arising from confusion of = and ==
 
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
+
+    for x in (1, 2, 3):
+        for y in (1, 2, 3):
+            mainframe.columnconfigure(x, weight=1)
+            mainframe.rowconfigure(y, weight=1)
+
     # The Tkinter tutorial says this is necessary
     # to make things scale or something. Idk
 
     ttk.Label(mainframe, text="Input", justify="center").grid(column=1, row=1, columnspan=2, sticky=NSEW)
-    ttk.Button(mainframe, text="Select file", command=browse_for_file).grid(column=1, row=2, sticky=NSEW)
-    
-    file_path = StringVar() # If-blocks don't create scope in Python, so no need for global keyword
-    (file_path_entry := ttk.Entry(mainframe, textvariable=file_path, width=20)).grid(column=2, row=2, sticky=NSEW)
 
-    file_path_entry.focus()
+    ttk.Button(mainframe, text="Select file", command=browse_for_file).grid(column=1, row=2, sticky=EW)
+    
+    file_path_in = StringVar(value="No file selected") # Variable obviously set to "No file selected"
+    
+    (file_path_entry := ttk.Entry(mainframe, textvariable=file_path_in, width=30)).grid(column=2, row=2, sticky=EW)
+    (file_path_label := ttk.Label(mainframe, textvariable=file_path_in, width=30)).grid(column=2, row=3, sticky=NSEW)
+
+    print(file_path_in.get()) # Prints "No file selected" as expected
+
+    # Entry and label still somehow display as blank for no good reason
+
     root.mainloop()
