72679a668b1ed5478ce10bdcbd4f8bfa8eb83341
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-01-03 19:14:19 -0500

    Finalized compilation function

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,3 +1,8 @@
+import tkinter
+from tkinter import filedialog
+import typing
+import os
+
 if __name__ == "__main__":
     from parser import parser
     from parser import post_parser
@@ -11,46 +16,42 @@ else:
     from .datapack_generator import datapack_generator
     from .datapack_generator import datapack_directory_management as ddm
 
-import tkinter
-from tkinter import filedialog
-
-def compile(src_file_path, destination_file_path) -> None:
-    raise NotImplementedError
-    
-    src = ""
-
-    with open(src_file_path) as f:
-        src = f.read()
-    ast = parser.parse(tokens)
-    ast2 = post_parser.post_parse(ast)
-    directory_rep = datapack_generator.generate_datapack(ast2)
-    directory_rep.realize(destination_file_path)
-
-if __name__ == "__main__":
-    root = tkinter.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename(filetypes=[("Orthophosphate Files", "*.txt *.opo4")])
+def compile(src_file_path: str, destination_file_path: typing.Optional[str], do_prints=False) -> None:
+    with open(src_file_path) as file:
+        src = file.read()
+    tokens = tokenizer.tokenize(src)
 
     SEPARATOR = "\n### ### ###\n"
-    LESSER_SEPERATOR = "\n--- --- ---\n"
 
-    with open(file_path, 'r') as file:
-        src = file.read()
-
-        tokens = tokenizer.tokenize(src)
-
+    if do_prints:
         print(SEPARATOR)
         print(src)
         print(SEPARATOR)
         print(tokens)
         print(SEPARATOR)
-        ast = parser.parse(tokens)
-        # print(repr(ast))
-        # print(LESSER_SEPERATOR)
+    ast = parser.parse(tokens)
+
+    if do_prints:
         print(ast)
         print(SEPARATOR)
-        ast2 = post_parser.post_parse(ast)
+    ast2 = post_parser.post_parse(ast)
+
+    if do_prints:
         print(ast2)
         print(SEPARATOR)
-        directory_rep = datapack_generator.generate_datapack(ast2)
+    
+    directory_rep = datapack_generator.generate_datapack(
+        ast2,
+        name=os.path.splitext(os.path.basename(src_file_path))[0]
+    )
+    if destination_file_path is not None:
+        directory_rep.realize(destination_file_path)
+    else:
         print(directory_rep)
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(filetypes=[("Orthophosphate Files", "*.opo4")])
+
+    compile(file_path, None, False)
