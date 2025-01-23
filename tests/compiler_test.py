097522a3d00f7daaee7af687d5690e4fc6e2cabd
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-01-23 12:16:53 -0500

    Testing things

--- tests/compiler_test.py
+++ tests/compiler_test.py
@@ -3,13 +3,28 @@ import os.path
 import src.orthophosphate.compiler.compiler as compiler
 import src.orthophosphate.compiler.datapack_generator.datapack_directory_management as ddm
 
-def compile_hello_world() -> ddm.FolderRep:
+TEST_SRC_PATH = os.path.join(os.path.abspath("."), "src", "orthophosphate", "compiler", "test_src_files")
+
+def compile_test_src(name: str, nameIncludesExtension: bool = False) -> ddm.FolderRep:
+    """
+    Name should not include the .opo4 extension unless
+    specified by boolean arg
+    """
     return compiler.pure_function_compile(
-            os.path.join(os.path.abspath("."), "src", "orthophosphate", "compiler", "test_src_files", "hello_world.opo4")
+            os.path.join(TEST_SRC_PATH, name + (".opo4" if not nameIncludesExtension else ""))
         )
 
-def test_compiler():
-    assert isinstance(
-        compile_hello_world(),
-        ddm.FolderRep
+def compile_hello_world() -> ddm.FolderRep:
+    return compile_test_src("hello_world")
+
+def compile_all() -> tuple[ddm.FolderRep, ...]:
+    return tuple(
+        compile_test_src(name, nameIncludesExtension=True) for name in os.listdir(TEST_SRC_PATH)
     )
+
+def test_types():
+    for item in compile_all():
+        assert isinstance(
+            item,
+            ddm.FolderRep
+        )
