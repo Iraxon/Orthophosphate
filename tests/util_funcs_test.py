3d23ed5d68d1f818b7f73fa5767a539861f327a1
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-01-23 12:29:52 -0500

    Added test location for util funcs

---
+++ tests/util_funcs_test.py
@@ -0,0 +1,10 @@
+import src.orthophosphate.compiler.datapack_generator.datapack_directory_management as ddm
+
+def test_namespacing():
+    for k,v in {
+        "My very cool DATAPACK": "my_very_cool_datapack",
+        "Maiestas": "maiestas",
+        "This.thing+" : "this.thing",
+        "Name Space 99": "name_space_99"
+        }.items():
+        assert ddm.namespacify(k) == v
