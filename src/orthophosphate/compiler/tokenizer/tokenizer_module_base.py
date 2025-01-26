42358cbc7da0491741a57603e075f393e7d73b01
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-01-26 11:45:10 -0500

    Added optimization in the case of 1-char tokens and error detection to tokenizer_module_base's default calculate() method

--- src/orthophosphate/compiler/tokenizer/tokenizer_module_base.py
+++ src/orthophosphate/compiler/tokenizer/tokenizer_module_base.py
@@ -18,8 +18,20 @@ class TokenizerModuleBase(typing.NamedTuple):
         OUTPUT: tokenizer output
         calculates the tokenizer output given the data
         """
-        token_string = ""
-        while cursor < len(data) and token_string + data[cursor] in TokenizerModuleBase.matches:
-            token_string += data[cursor]
+        if len(TokenizerModuleBase.matches) > 1:
+            token_string = ""
+            while cursor < len(data) and token_string + data[cursor] in TokenizerModuleBase.matches:
+                token_string += data[cursor]
+                cursor += 1
+            return cursor, compiledTokens, data
+
+        if len(TokenizerModuleBase.matches) == 1:
+            if compiledTokens[cursor] not in TokenizerModuleBase.matches:
+                raise ValueError(
+                    f"Calculate called for single-character token {compiledTokens[cursor]}, "
+                    f"which is not a match for {TokenizerModuleBase}; "
+                    f"matches: {TokenizerModuleBase.matches}"
+                )
             cursor += 1
-        return cursor, compiledTokens, data
+            return cursor, compiledTokens, data
+        raise ValueError(f"{TokenizerModuleBase} has zero listed matches")
