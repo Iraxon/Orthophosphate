bc9f134eddb25fd237f23e0b54aac9d1942caa6b
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-06-08 10:07:45 -0400

    Fixed type shenanigans

--- src/orthophosphate/compiler/parser/post_parser.py
+++ src/orthophosphate/compiler/parser/post_parser.py
@@ -1,20 +1,21 @@
+import typing
 from . import abstract_syntax_tree as tree
 
-def strip_empties(ast: tree.Node) -> tree.Node:
+def strip_empties(ast: tree.Node | None) -> tree.Node | None:
     """
     Strip empty STATEMENT nodes from the AST
     """
     if ast is None:
         return None
     # If AST.value is a tuple and that tuple is empty, return None
-    if ast.type in frozenset((tree.NodeType.STATEMENT, tree.NodeType.PREFIX_EXPRESSION)) and isinstance(ast.value, tuple) and len(ast.value) == 0:
+    if ast.type in frozenset((tree.NodeType.STATEMENT, tree.NodeType.EXPRESSION)) and isinstance(ast.value, tuple) and len(ast.value) == 0:
         return None
     return ast
 
 def identity(x):
     return x
 
-def simple_pass(node_in: tree.Node, f=identity) -> tree.Node:
+def simple_pass(node_in: tree.Node, f: typing.Callable[..., tree.Node | None]=identity) -> tree.Node | None:
     """
     Does a pass over the tree, building
     a new tree by passing every node
@@ -23,25 +24,30 @@ def simple_pass(node_in: tree.Node, f=identity) -> tree.Node:
     if no function is provided, the identity
     function is used
     """
-    if node_in is None:
-        return None
-    if isinstance(node_in.value, tuple):
-        node_mid = f(node_in)
-        if node_mid is None:
+    match node_in, node_in.value:
+        case None, _:
             return None
-        value = tuple(
-            simple_pass(item, f)
-            for item in node_mid.value
-            if simple_pass(item, f) is not None
-        )
-        return tree.Node(
-            type=node_mid.type,
-            value=value,
-            data_type=node_mid.data_type
-        )
-    return f(node_in)
+        case node, value if isinstance(value, tuple):
+            node_mid = f(node)
+            if node_mid is None:
+                return None
+            next_value = typing.cast(
+                tuple[tree.Node, ...],
+                tuple(
+                    simple_pass(item, f)
+                    for item in value
+                    if simple_pass(item, f) is not None
+                )
+            )
+            return tree.Node(
+                type=node_mid.type,
+                value=next_value,
+                data_type=node_mid.data_type
+            )
+        case _:
+            return f(node_in)
 
-def _move_up_loose_functions(nodes: tuple[tree.Node]) -> tuple[tree.Node]:
+def _move_up_loose_functions(nodes: tuple[tree.Node, ...]) -> tuple[tree.Node, ...]:
     """
     Private function that shoves loose items into the default namespace
     """
@@ -57,14 +63,14 @@ def _move_up_loose_functions(nodes: tuple[tree.Node]) -> tuple[tree.Node]:
     return (
         (
             tree.Node(
-                type="namespace",
+                type=tree.NodeType.NAMESPACE,
                 value=(
                     tree.Node(
-                        type="placeholder",
+                        type=tree.NodeType.PLACEHOLDER,
                         value="default_namespace"
                     ),
                     tree.Node(
-                        type="block",
+                        type=tree.NodeType.BLOCK,
                         value=tuple(contents_of_default_namespace)
                     )
                 )
@@ -80,7 +86,7 @@ def expand_tree(ast: tree.Node) -> tree.Node:
     """
     new_ast = tree.Node(
         type=ast.type,
-        value=_move_up_loose_functions(nodes=ast.value),
+        value=_move_up_loose_functions(nodes=typing.cast(tuple[tree.Node, ...], ast.value)),
         data_type=ast.type
     )
     return new_ast
@@ -98,7 +104,7 @@ def post_parse(ast: tree.Node) -> tree.Node:
     Transforms the AST to make it suitable
     for passing to the datapack generator
     """
-    stripped_ast = simple_pass(ast, strip_empties)
+    stripped_ast = typing.cast(tree.Node, simple_pass(ast, strip_empties))
     expanded_ast = expand_tree(stripped_ast)
-    post_stripped_ast = simple_pass(expanded_ast, strip_empties)
+    post_stripped_ast = typing.cast(tree.Node, simple_pass(expanded_ast, strip_empties))
     return post_stripped_ast
