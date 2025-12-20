ed5369bf3553aeb347a1043fe79ad0f084da9120
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-12-19 19:08:11 -0500

    Parser rewrite

--- src/orthophosphate/compiler/tokenizer/Tokenizer.py
+++ src/orthophosphate/compiler/tokenizer/Tokenizer.py
@@ -1,8 +1,9 @@
+from enum import Enum, auto
+import string
 import typing
 
 from .tokenizer_module_base import TokenizerModuleBase
 from .token import Token, TokenType
-from .tokens import TOKEN_MODULES, PunctuationToken, WhiteSpaceToken
 
 # NEEDED CHANGES:
 # finish class lol
@@ -23,72 +24,128 @@ def get_ln_and_col(s: str, index) -> tuple[int, int]:
     return len(lines), col
 
 
-# helper data set. Maps every token string to the token it is a part of
-token_strings: dict[
-    str, typing.Callable[[int, list[Token], str], tuple[int, list[Token], str]]
-] = {
-    match: token.calculate
-    for token in TOKEN_MODULES  # this is defined in token_modules
-    for match in token.matches
-}
-
-
 def tokenize(input: str) -> tuple[Token, ...]:
 
-    data: str = input
+    output: list[Token] = []
 
-    # where the tokenizer is in the data
-    cursor: int = 0
+    i: int = 0
+    j: int = 0
+    current_str: str
+    matched_str: str
+    possible_token: Token | SpecialCase | None
 
-    # the current token. This is needed because most tokens are more than one character long
-    token: str = ""
+    output.append(Token(TokenType.PUNC, "file_start"))
 
-    # the currently compiled tokens
-    compiledTokens: list[Token] = []
+    while i < len(input):
 
-    # Invisible punctuation added to start of file
-    compiledTokens.append(Token(TokenType.PUNC, "file_start"))
+        j = i + 1
+        current_str = ""
+        matched_str = ""
+        possible_token = None
 
-    # logic
-    while cursor < len(data):
-        token = data[cursor]
+        while j < len(input) + 1:  # j is an exclusive range closer
 
-        if token not in WhiteSpaceToken.matches:  # We don't do anything for whitespace
+            current_str = input[i:j]
+            char = current_str[-1]  # The thing just added
 
-            # Look ahead with an inner loop
-            # and try to see what tokens
-            # this character could be the start of
-            possible_tokens: list[str] = []
-            inner_token = ""
-            inner_cursor = cursor
-            while (inner_cursor < len(data)) and (
-                data[inner_cursor] not in WhiteSpaceToken.matches
-            ):
-                inner_token += data[inner_cursor]
-                if inner_token in token_strings:
-                    possible_tokens.append(inner_token)
-                inner_cursor += 1
-                # print(f"Inner tokenizer loop running: {cursor}:{inner_cursor} on {inner_token}")
-            # print(possible_tokens)
+            if char.isspace() and not needs_whitespace(current_str):
+                if not current_str.isspace():
+                    # We hit whitespace after a token, and we don't need the whitespace, so we're done
+                    break
+                else:
+                    # The whole thing is whitespace, so we report that to properly skip it
+                    matched_str = current_str
+                    possible_token = SpecialCase.WHITESPACE
+                    break
 
-            # Decide on which token module to get calculate() from
-            if len(possible_tokens) >= 1:
-                chosen_token = sorted(possible_tokens, key=len, reverse=True)[
-                    0
-                ]  # Get the longest of the possible tokens
-            else:
-                raise ValueError(f"Token {token} has no matches")
+            match token_of(current_str):
+                case Token() | SpecialCase() as t:
+                    matched_str = current_str
+                    possible_token = t
+                case None:
+                    pass  # Invalid token; don't overwrite
 
-            # Apply calculate()
-            cursor, compiledTokens, data = token_strings[chosen_token](
-                cursor, compiledTokens, data
+            print(
+                f"{i}:{j}, over <{input[i:j].replace("\n", "\\n")}>, with str {current_str}"
             )
-            # print(compiledTokens[-1])
-        cursor += 1
-        # print(f"Outer tokenizer loop running: {cursor} on {token}")
-    compiledTokens.append(Token(TokenType.PUNC, "EOF"))
+            j += 1
 
-    return tuple(compiledTokens)
+        if possible_token is None:
+            raise ValueError(
+                f"No token match for {current_str} or any fragment thereof; last token: {output.pop()}"
+                # Never an index error because of file_start Token
+            )
+        if isinstance(possible_token, Token):
+            print(f"Matched {matched_str} -> {possible_token}")
+            output.append(possible_token)
+        i += len(matched_str)
+
+    output.append(Token(TokenType.PUNC, "EOF"))  # End of file
+
+    return tuple(output)
+
+
+def is_whitespace(s: str) -> bool:
+    return s.isspace()
+
+
+def needs_whitespace(s: str) -> bool:
+    """
+    Returns whether this token fragment
+    indicates that the tokenizer has to check
+    for match possibilities that include whitespace.
+
+    (i.e. Could this be the beginning of a string or comment?)
+    """
+    return any(s.startswith(prefix) for prefix in ('"', "//", "/*", "#"))
+
+
+class SpecialCase(Enum):
+    COMMENT = auto()
+    WHITESPACE = auto()  # Not used by token_of() but by lexing logic directly
+
+
+def token_of(s: str) -> Token | SpecialCase | None:
+    """
+    Returns the type of Token s is, or
+    None if its not a valid token.
+
+    Returns SpecialCase.COMMENT if this token is a comment.
+    """
+
+    if s.isdigit():
+        return Token(TokenType.INT, s)
+
+    if len(s) >= 2 and s.startswith('"') and s.endswith('"'):
+        return Token(TokenType.STR, s[1:-1])
+
+    if ((s.startswith("#") or s.startswith("//")) and s.endswith("\n")) or (
+        s.startswith("/*") and s.endswith("*/")
+    ):
+        return SpecialCase.COMMENT
+
+    match s:
+
+        # Punctuation
+        case (";" | "(" | ")" | "{" | "}" | "[" | "]" | "$" | "->") as p:
+            return Token(TokenType.PUNC, p)
+
+        # Name token or invalid
+        case _:
+
+            if (
+                s != ""
+                and s[0] in NAME_MATCHES
+                and all(char in NAME_CAN_CONTAIN for char in s)
+            ):
+                return Token(TokenType.NAME, s)
+
+            # Not a valid token
+            return None
+
+
+NAME_MATCHES = string.ascii_letters + "_$"
+NAME_CAN_CONTAIN = NAME_MATCHES + string.digits + ".:/"
 
 
 # helper function to print all the tokens
