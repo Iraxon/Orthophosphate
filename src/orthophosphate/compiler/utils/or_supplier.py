09c2e90e63d321f3acc2f4b9fc4b85217154f7b8
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-05-17 11:28:24 -0400

    Capitalization

--- src/orthophosphate/compiler/utils/or_supplier.py
+++ src/orthophosphate/compiler/utils/or_supplier.py
@@ -1,13 +1,13 @@
 from collections.abc import Callable
 
 
-type orSupplier[T] = T | Callable[[], T]
+type OrSupplier[T] = T | Callable[[], T]
 """
 T must NOT be a Callable type
 """
 
 
-def get[T](v: orSupplier[T]) -> T:
+def get[T](v: OrSupplier[T]) -> T:
     if callable(v):
         return v() # type: ignore ; This errors for the case that T is a Callable itself
     else:
