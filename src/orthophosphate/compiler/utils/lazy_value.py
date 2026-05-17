d26c649dee6322e1028778a0d1c9695551f9c7f3
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-05-17 11:13:19 -0400

    Improved type hinting

--- src/orthophosphate/compiler/utils/lazy_value.py
+++ src/orthophosphate/compiler/utils/lazy_value.py
@@ -1,9 +1,22 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from enum import Enum, auto
+from functools import cache
 
+class _NoValue(Enum):
+    NO_VALUE = auto()
 
-def lazy_value[T](supplier: Callable[[], T]) -> Callable[[], T]:
+@dataclass
+class Lazy[T]:
+    supplier: Callable[[], T]
+    _cached_value: T | _NoValue = field(init=False, default=_NoValue.NO_VALUE)
+
+    def __call__(self) -> T:
+        if self._cached_value is _NoValue.NO_VALUE:
+            self._cached_value = self.supplier()
+        return self._cached_value
+@cache
+def lazy_of[T](supplier: Callable[[], T]) -> Lazy[T]:
     """
     Makes a lazy value from the given supplier; the lazy value
     uses the supplier the first time it is called, then uses
@@ -15,17 +28,3 @@ def lazy_value[T](supplier: Callable[[], T]) -> Callable[[], T]:
     :rtype: Callable[[], T]
     """
     return Lazy(supplier)
-
-
-class _NoValue(Enum):
-    NO_VALUE = auto()
-
-@dataclass
-class Lazy[T]:
-    supplier: Callable[[], T]
-    _cache: T | _NoValue = field(init=False, default=_NoValue.NO_VALUE)
-
-    def __call__(self) -> T:
-        if self._cache is _NoValue.NO_VALUE:
-            self._cache = self.supplier()
-        return self._cache
