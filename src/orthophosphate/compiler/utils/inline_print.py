54157940e536b2659c577e9243b323a02e76212b
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-05-19 11:14:15 -0400

    Added support prefixes

--- src/orthophosphate/compiler/utils/inline_print.py
+++ src/orthophosphate/compiler/utils/inline_print.py
@@ -1,6 +1,8 @@
 from collections.abc import Callable
 
 
-def inline_print[T](x: T, print_msg: Callable[[T], str] | str = str) -> T:
-    print(print_msg(x) if callable(print_msg) else print_msg)
+def inline_print[T](
+    x: T, prefix: str = "", print_msg: Callable[[T], str] | str = str
+) -> T:
+    print(f"{prefix}{print_msg(x) if callable(print_msg) else print_msg}")
     return x
