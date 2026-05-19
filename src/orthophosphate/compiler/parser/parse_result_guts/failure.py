1955b895c3a97c3f56653b80dc99548927c30750
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-05-19 00:02:28 -0400

    Added methods for expanding on errors

--- src/orthophosphate/compiler/parser/parse_result_guts/failure.py
+++ src/orthophosphate/compiler/parser/parse_result_guts/failure.py
@@ -1,9 +1,21 @@
 from dataclasses import dataclass
-
-from ...utils.frozeniter import FrozenIter
+from typing import Self
 
 
 @dataclass(frozen=True)
-class Failure[SRC]:
+class Failure:
     errors: tuple[str, ...]
-    src_iter: FrozenIter[SRC]
+
+    def elaborate_on(self, new_info: str) -> Self:
+        """
+        For when the info in self.errors is the
+        reason behind new_info
+        """
+        return type(self)((new_info, *self.errors))
+
+    def with_reasons(self, reasons: tuple[str, ...]) -> Self:
+        """
+        For when you have reasons that describe
+        the why behind the info in self.errors
+        """
+        return type(self)((*self.errors, *reasons))
