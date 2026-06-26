ecf1b439736443eef81524b7676a56569be25a78
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-06-26 12:24:32 -0400

    Safety commit from before ParseStack overhaul

--- src/orthophosphate/compiler/parser/multistage_parser.py
+++ src/orthophosphate/compiler/parser/multistage_parser.py
@@ -1,24 +1,190 @@
-from collections.abc import Iterable
+from collections.abc import Callable, Iterable, Sequence
+from dataclasses import dataclass
+from typing import Any, Never, Self
 
-from .parse_tree import LiteralNode, PyLiteralNode
+from ..tokenizer.token import IndentType, Token, TokenType
+from .parse_tree2 import (
+    AnyInlineExpr,
+    AnyMultilineExpr,
+    InlineExpr,
+    ParenthesizedInlineExprSeq,
+    IntLiteral,
+    ListLiteral,
+    MultilineExpr,
+    NewlineInlineExprSeq,
+    MultilineExprSeq,
+    Name,
+    ParseTreeNode,
+    StrLiteral,
+)
 
-from ..tokenizer.token import Token, TokenType
+type IntermediaryParseResult = Token | ParseTreeNode
 
 
-def parse_one_token_literal(src: Iterable[Token]) -> tuple[Token | LiteralNode, ...]:
-    def process_one(token: Token) -> Token | LiteralNode:
-        match token.type:
-            case TokenType.STR:
-                return PyLiteralNode(str, token.value)
-            case TokenType.INT:
-                return PyLiteralNode(int, int(token.value))
-            case _:
-                return token
+@dataclass(frozen=True)
+class ParseStack:
+    previous: Self | None
+    current: IntermediaryParseResult
 
-    return tuple(process_one(token) for token in src)
+    def pop(self) -> tuple[IntermediaryParseResult, Self | None]:
+        return self.current, self.previous
+
+    def push(self, result: IntermediaryParseResult):
+        return type(self)(self, result)
+
+    shift = push
 
 
-def parse(src: Iterable[Token]) -> ...:
-    result = parse_one_token_literal(src)
-    print(result)
+def reduce_literals(
+    literal: IntermediaryParseResult,
+    *_: Any,
+) -> IntermediaryParseResult | None:
+    if isinstance(literal, Token):
+        if literal.type is TokenType.INT:
+            return IntLiteral(int(literal.value))
+        if literal.type is TokenType.STR:
+            return StrLiteral(literal.value)
+        if literal.type is TokenType.NAME:
+            return Name(literal.value)
+    return None
+
+
+def reduce_inline_expr_seq(
+    first: IntermediaryParseResult,
+    second: IntermediaryParseResult,
+    *_: Any,
+) -> IntermediaryParseResult | None:
+
+    if isinstance(first, AnyInlineExpr):
+        if isinstance(second, ParenthesizedInlineExprSeq) or isinstance(
+            second, NewlineInlineExprSeq
+        ):
+            return second.prepend(first)
+        elif isinstance(second, Token):
+            if second.matches(TokenType.PUNC, ")"):
+                return ParenthesizedInlineExprSeq(first, None)
+            elif second.type is TokenType.NEWLINE:
+                return NewlineInlineExprSeq(first, None)
+    return None
+
+
+def reduce_multiline_expr_seq(
+    first: IntermediaryParseResult, second: IntermediaryParseResult, *_: Any
+) -> IntermediaryParseResult | None:
+    if isinstance(first, AnyMultilineExpr):
+        if isinstance(second, MultilineExprSeq):
+            return second.prepend(first)
+        elif isinstance(second, Token) and (
+            second.matches(TokenType.PUNC, "]")
+            or second.matches(TokenType.INDENT_DEDENT, IndentType.DEDENT)
+        ):
+            return MultilineExprSeq(first, None)
+    return None
+
+
+def reduce_list(
+    left_paren: IntermediaryParseResult,
+    contents: IntermediaryParseResult,
+    *_: Any,
+) -> IntermediaryParseResult | None:
+    if (
+        isinstance(left_paren, Token)
+        and left_paren.matches(TokenType.PUNC, "[")
+        and isinstance(contents, MultilineExprSeq)
+    ):
+        return ListLiteral(tuple(contents))
+    return None
+
+
+def reduce_inline_expr(
+    first: IntermediaryParseResult,
+    left_paren: IntermediaryParseResult,
+    args: IntermediaryParseResult,
+    *_: Any,
+) -> IntermediaryParseResult | None:
+    if (
+        isinstance(first, AnyInlineExpr)
+        and isinstance(left_paren, Token)
+        and left_paren.matches(TokenType.PUNC, "(")
+        and isinstance(args, ParenthesizedInlineExprSeq)
+    ):
+        return InlineExpr(first, tuple(args))
+    return None
+
+
+def reduce_expr_with_indented_args(
+    inline_args: IntermediaryParseResult,
+    indent: IntermediaryParseResult,
+    indented_args: IntermediaryParseResult,
+    *_: Any,
+) -> IntermediaryParseResult | None:
+    if (
+        isinstance(inline_args, ParenthesizedInlineExprSeq)
+        and isinstance(indent, Token)
+        and indent.matches(TokenType.INDENT_DEDENT, IndentType.INDENT)
+        and isinstance(indented_args, MultilineExprSeq)
+    ):
+        return MultilineExpr(tuple(inline_args), tuple(indented_args))
+    return None
+
+
+reductions: Sequence[
+    tuple[
+        Callable[
+            [*tuple[IntermediaryParseResult, ...]], IntermediaryParseResult | None
+        ],
+        int,
+    ]
+] = (
+    (reduce_literals, 1),
+    (reduce_inline_expr_seq, 2),
+    (reduce_list, 3),
+    (reduce_inline_expr, 4),
+    (reduce_multiline_expr_seq, 2),
+    (reduce_expr_with_indented_args, 5),
+)
+
+
+def apply_reductions(
+    parse_stack: ParseStack,
+) -> ParseStack | None:
+
+    for reduction, length in reductions:
+        if len(parse_stack) >= length:
+            last = parse_stack[-length:]
+            result = reduction(*last)
+            if result is not None:
+                return (*parse_stack[:-length], result)
+
+    return None
+
+
+def display_intermediate_result(r: IntermediaryParseResult) -> str:
+    if isinstance(r, Sequence) and not isinstance(r, str):
+        return f"<{" ".join(map(str, r))}>"
+    return str(r)
+
+
+def parse(src: Iterable[Token]) -> Never:
+    parse_stack: ParseStack = tuple()
+    print(parse_stack)
+    for token in src:
+        parse_stack = parse_stack.shift(token)
+        if len(s := " : ".join(map(display_intermediate_result, parse_stack))) < 400:
+            print(s)
+        while True:
+            inner_parse_stack = apply_reductions(parse_stack)
+            if inner_parse_stack is None:
+                break
+            else:
+                if (
+                    len(
+                        s := " : ".join(
+                            map(display_intermediate_result, inner_parse_stack)
+                        )
+                    )
+                    < 400
+                ):
+                    print(s)
+                parse_stack = inner_parse_stack
     raise NotImplementedError(f"See the so-far-implemented output above traceback.")
---
+++ src/orthophosphate/compiler/parser/parse_tree2.py
@@ -0,0 +1,244 @@
+from abc import abstractmethod
+from collections.abc import Iterable, Iterator, Sequence
+from dataclasses import dataclass
+from functools import cache
+from typing import Protocol, Self, cast, override
+
+type AnyExprNode = AnyMultilineExpr | AnyInlineExpr | ListLiteral
+"""
+A node on the parse tree that stands for a real expression
+"""
+
+
+@dataclass(frozen=True)
+class ParseTreeNode(Protocol):
+    """
+    Only exists to override __str__ for all the nodes
+
+    Use type alias ParseTreeNode for literally all practical purposes
+    """
+
+    @override
+    def __str__(self) -> str:
+        return _inline_display(cast(ParseTreeNode, self))
+
+    @abstractmethod
+    def get_render_contents(self) -> "tuple[str, Sequence[ParseTreeNode | str]]":
+        raise NotImplementedError
+
+    def inline_display(self) -> str:
+        head, args = self.get_render_contents()
+        if len(args) > 0:
+            args_display = " ".join(
+                arg if isinstance(arg, str) else arg.inline_display() for arg in args
+            )
+            return f"{head}({args_display})"
+        return head
+
+
+@dataclass(frozen=True)
+class Program(ParseTreeNode):
+    content: Sequence[AnyExprNode]
+
+
+@dataclass(frozen=True)
+class AnyMultilineExpr(ParseTreeNode): ...
+
+
+@dataclass(frozen=True)
+class MultilineExpr(AnyMultilineExpr):
+    inline_args: "Sequence[AnyInlineExpr]"
+    args: Sequence[AnyMultilineExpr]
+
+    @override
+    def get_render_contents(self):
+        combined = (*self.inline_args, r"/n", *self.args)
+        return (_inline_display(combined[0]), combined[1:])
+
+
+@dataclass(frozen=True)
+class ListLiteral(AnyMultilineExpr):
+    content: Sequence[AnyMultilineExpr]
+
+    @override
+    def get_render_contents(self):
+        return ("list", self.content)
+
+    @override
+    def inline_display(self) -> str:
+        return f"[{" ".join(arg.inline_display() for arg in self.content)}]"
+
+
+@dataclass(frozen=True)
+class AnyInlineExpr(ParseTreeNode): ...
+
+
+@dataclass(frozen=True)
+class InlineExpr(AnyInlineExpr):
+    head: AnyInlineExpr
+    contents: Sequence[AnyInlineExpr]
+
+    @override
+    def get_render_contents(self):
+        return (self.head.inline_display(), self.contents)
+
+
+@dataclass(frozen=True)
+class BottomLiteralNode(AnyInlineExpr):
+    value: int | str
+
+    @override
+    def get_render_contents(self):
+        return (str(self.value), ())
+
+
+@dataclass(frozen=True)
+class Name(BottomLiteralNode):
+    value: str
+
+
+@dataclass(frozen=True)
+class IntLiteral(BottomLiteralNode):
+    value: int
+
+
+@dataclass(frozen=True)
+class StrLiteral(BottomLiteralNode):
+    value: str
+
+
+@dataclass(frozen=True)
+class AnyNodeSeq(ParseTreeNode):
+
+    content: ParseTreeNode
+    next: Self | None
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[ParseTreeNode]:
+        raise NotImplementedError
+
+    @override
+    def get_render_contents(self) -> tuple[str, tuple[ParseTreeNode, ...]]:
+        return ("<>", tuple(self))
+
+    @override
+    def inline_display(self) -> str:
+        return f"<{" ".join(arg.inline_display() for arg in iter(self))}>"
+
+
+@dataclass(frozen=True)
+class MultilineExprSeq(AnyNodeSeq, Iterable[AnyMultilineExpr]):
+    content: AnyMultilineExpr
+    next: Self | None
+
+    def prepend(self, content: AnyMultilineExpr) -> Self:
+        return type(self)(content, self)
+
+    @override
+    def __iter__(self) -> Iterator[AnyMultilineExpr]:
+        current = self
+        while current is not None:
+            yield current.content
+            current = current.next
+
+
+@dataclass(frozen=True)
+class ParenthesizedInlineExprSeq(AnyNodeSeq, Iterable[AnyInlineExpr]):
+    content: AnyInlineExpr
+    next: Self | None
+
+    def prepend(self, content: AnyInlineExpr) -> Self:
+        return type(self)(content, self)
+
+    @override
+    def __iter__(self) -> Iterator[AnyInlineExpr]:
+        current = self
+        while current is not None:
+            yield current.content
+            current = current.next
+
+
+@dataclass(frozen=True)
+class NewlineInlineExprSeq(AnyNodeSeq, AnyMultilineExpr, Iterable[AnyInlineExpr]):
+    """
+    A multiline expr without indented args.
+    Unlike an inline expr, this ends in a newline
+    and may have multiple inline args.
+    """
+
+    content: AnyInlineExpr
+    next: Self | None
+
+    def add_indented_args(
+        self, indented_args: Sequence[AnyMultilineExpr]
+    ) -> MultilineExpr:
+        return MultilineExpr(tuple(self), indented_args)
+
+    def prepend(self, content: AnyInlineExpr) -> Self:
+        return type(self)(content, self)
+
+    @override
+    def get_render_contents(self):
+        args = tuple(self)
+        return (_inline_display(args[0]), args[1:])
+
+    @override
+    def __iter__(self) -> Iterator[AnyInlineExpr]:
+        current = self
+        while current is not None:
+            yield current.content
+            current = current.next
+
+
+@cache
+def _render_contents(
+    node: ParseTreeNode | str,
+) -> tuple[str, Sequence[ParseTreeNode | str]]:
+    """
+    Provides the header and children that a
+    Node should display when rendered (see below
+    _display_node function)
+    """
+    if isinstance(node, str):
+        return (node, ())
+    return node.get_render_contents()
+
+
+@cache
+def _display_node(node: ParseTreeNode | str, pre: str = "") -> str:
+    """
+    Provides a nice readable string
+    rep of the Node with nesting
+
+    This function is recursive and
+    dangerous to the sanity of anyone
+    who works on it
+    """
+    header, children = _render_contents(node)
+
+    render_contents: tuple[str, ...] = tuple(
+        _display_node(child, pre + ("║ " if i < len(children) - 1 else "  "))
+        for i, child in enumerate(children)
+    )
+
+    return f"{'' if pre == '' else'═'} {header}\n" + "".join(
+        (
+            f"{pre}╠{element}"  # Normal case
+            if i < len(render_contents) - 1
+            else f"{pre}╚{element}"  # Last element
+        )
+        for i, element in enumerate(render_contents)
+    )
+
+
+@cache
+def _inline_display(node: ParseTreeNode | str) -> str:
+    """
+    Produces an inline representation (e.g. "head(arg1 arg2 arg3)")
+
+    Does not handle indentation
+    """
+
+    if isinstance(node, str):
+        return node
+    return node.inline_display()
